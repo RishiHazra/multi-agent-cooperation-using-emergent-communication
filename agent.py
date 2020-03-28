00c8b93cc9e083139463405f5b0346f9fa66a6f1
RishiHazra <36856187+RishiHazra@users.noreply.github.com>
2020-03-28 21:33:58 +0530

    Gumbel Softmax instead of Categorical for actions
    added a differentiable environment with ST gumbel softmax.

--- agent.py
+++ agent.py
@@ -1,38 +1,68 @@
-import torch
-import torch.nn as nn
-from torch.distributions import Categorical
-
-from arguments import Arguments
-from agent_model import ImgEncoder, Receiver, Sender, Decoder
+from agent_model import *
 
 args = Arguments()
+
+
+def weights_init(module):
+    """
+    initializer of Linear weights
+    """
+    if isinstance(module, nn.Linear):
+        nn.init.xavier_uniform_(module.weight, gain=1)
+        # nn.init.constant_(module.bias, 0)
 
 
 class Agent(nn.Module):
     def __init__(self, traits):
         super(Agent, self).__init__()
         self.img_encoder = ImgEncoder()
+        self.text_encoder = CharEncoder()
         self.receiver = Receiver()
         self.sender = Sender()
         self.decoder = Decoder()
-        self.softmax = nn.Softmax(dim=0)
+        self.softmax = nn.Softmax(dim=-1)
         self.traits = traits
-        self.resource_division = nn.Linear(args.num_agents-1, args.num_agents-1)
+        self.resource_division = nn.Linear(args.num_agents - 1, args.num_agents - 1)
         self.msgs_hidden = torch.zeros(1, args.msg_v_dim)
+        if args.flag == 1:  # text model
+            self.char_hidden = torch.zeros(1, args.encoded_size)
+        self.apply(weights_init)
 
     def reset_agent(self, traits):
         self.msgs_hidden = torch.zeros(1, args.msg_v_dim)
+        if args.flag == 1:  # text model
+            self.char_hidden = torch.zeros(1, args.encoded_size)
         self.traits = traits
 
-    def forward(self, image, msgs_input):
-        img_enc = self.img_encoder(image)
-        pooled, _ = self.receiver(msgs_input, self.traits[1].clone())
-        self.msgs_hidden, msgs_broadcast = \
-            self.sender(pooled, self.msgs_hidden, img_enc, self.traits[0].clone())
+    def forward(self, observation, msgs_input):
+        """
+        :param observation: partial observation of agent
+        :param msgs_input: input messages from other agents
+        :return: modified traits (resource division), action taken,
+                log_probabilities of all actions, broadcast messages
+        """
+        # encode observation
+        if args.flag == 0:  # encode images
+            enc = self.img_encoder(observation)
+        else:  # encode text
+            self.char_hidden = self.text_encoder(observation, self.char_hidden)
 
-        log_probs = self.decoder(img_enc, pooled)
-        probs = torch.exp(log_probs)
-        action = Categorical(probs=probs).sample()
-        log_probs = log_probs.gather(1, action.view(-1, 1)).view(-1)
+        # message pooling
+        pooled, _ = self.receiver(msgs_input, self.traits[1].clone())
+
+        # resource division
         self.traits[0] = self.softmax(self.resource_division(self.traits[0].clone().unsqueeze(0)))[0]
-        return self.traits, action, log_probs, msgs_broadcast
+
+        # broadcast messages
+        if args.flag == 0:
+            self.msgs_hidden, msgs_broadcast = \
+                self.sender(pooled, self.msgs_hidden, enc, self.traits[0].clone())
+            # take actions using Straight-Through Gumbel Softmax
+            action, log_probs, all_classes_log_probs = self.decoder(enc, pooled)
+        else:
+            self.msgs_hidden, msgs_broadcast = \
+                self.sender(pooled, self.msgs_hidden, self.char_hidden, self.traits[0].clone())
+            # take actions using Straight-Through Gumbel Softmax
+            action, log_probs, all_classes_log_probs = self.decoder(self.char_hidden, pooled)
+
+        return self.traits, action, log_probs, msgs_broadcast, all_classes_log_probs

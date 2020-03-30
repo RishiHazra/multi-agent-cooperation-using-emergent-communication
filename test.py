357c5c9a0ad3fec77403a06803b6fdb6a427973a
RishiHazra <36856187+RishiHazra@users.noreply.github.com>
2020-03-30 19:35:17 +0530

    added save embedding fn using tensorboardX

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 """
 test file for images
 """
+from tensorboardX import SummaryWriter
 
-import gc
 import os
-import pickle as pkl
+from tqdm import tqdm
+import pickle
 import torch
 import numpy as np
 import utils
@@ -13,12 +14,17 @@ from agent import Agent
 from arguments import Arguments
 
 args = Arguments()
+
+# file paths to be loaded
 model_path = 'model_with_L1'
 traits_file = open('traits_with_L1.pkl', 'rb')
 
+# Initialize the summary writer
+writer = SummaryWriter()
+
 
 def instantiate_agents(num_step):
-    agent_traits = pkl.load(traits_file)
+    agent_traits = pickle.load(traits_file)
 
     agent_nws = dict([(agent, Agent(agent_traits[agent]))
                       for agent in range(args.num_agents)])
@@ -29,9 +35,17 @@ def instantiate_agents(num_step):
     return agent_nws, agent_traits
 
 
+def save_embeddings(msg, true_label, agent, episode, policy_number):
+    num_time_steps = len(msg)
+    writer.add_embedding(msg,
+                         metadata=[agent] * num_time_steps,
+                         tag='Im_msg_' + str(agent) + '_EP_' +
+                             str(episode) + '_lab_' + str(true_label) + '_' + str(policy_number))
+
+
 if __name__ == '__main__':
-    step = 16000  # model step to be loaded
-    agents, traits_agents = instantiate_agents(step)
+    policy_step = 16000  # model step to be loaded
+    agents, traits_agents = instantiate_agents(policy_step)
     # calculate total reputation by summing all the 0th elements of the reputation lists
     total_reputation = torch.tensor(0.0)
     for agent_id in range(args.num_agents):
@@ -45,80 +59,86 @@ if __name__ == '__main__':
     num_consensus = 0  # number of times the community reaches consensus
     running_rewards = 0.0
 
-    for epoch in range(3):
-        for episode_id in range(args.num_test_episodes):
-            # pick up one sample from test dataset
-            (img, target) = test_set[episode_id]
-            img = img.unsqueeze(0)
-            target = torch.tensor([target])
+    for episode_id in tqdm(range(args.num_test_episodes)):
+        # pick up one sample from test dataset
+        (img, target) = test_set[episode_id]
+        img = img.unsqueeze(0)
+        target = torch.tensor([target])
 
-            # initialize / re-initialize all parameters
-            traits = [traits_agents[agent_id].clone() for agent_id in range(args.num_agents)]
-            agent_rewards = dict([(agent_id, []) for agent_id in range(args.num_agents)])
-            msgs_input = dict([(agent_id, torch.zeros(args.num_agents - 1, args.msg_v_dim + args.msg_k_dim))
+        # initialize / re-initialize all parameters
+        traits = [traits_agents[agent_id].clone() for agent_id in range(args.num_agents)]
+        agent_rewards = dict([(agent_id, []) for agent_id in range(args.num_agents)])
+        msgs_input = dict([(agent_id, torch.zeros(args.num_agents - 1, args.msg_v_dim + args.msg_k_dim))
+                           for agent_id in range(args.num_agents)])
+        msgs_broadcast = dict([(agent_id, torch.zeros(args.num_agents - 1, args.msg_v_dim))
                                for agent_id in range(args.num_agents)])
-            msgs_broadcast = dict([(agent_id, torch.zeros(args.num_agents - 1, args.msg_v_dim))
-                                   for agent_id in range(args.num_agents)])
 
-            # reset agent hidden vectors of LSTM and traits to original values
+        save_msgs_broadcast = dict([(agent_id, []) for agent_id in range(args.num_agents)])
+
+        # reset agent hidden vectors of LSTM and traits to original values
+        for agent_id in range(args.num_agents):
+            agents[agent_id].reset_agent(traits[agent_id])
+            agents[agent_id].eval()
+
+        # begin episode
+        for step in range(args.num_steps):
+            consensus = [0] * args.num_classes
             for agent_id in range(args.num_agents):
-                agents[agent_id].reset_agent(traits[agent_id])
-                agents[agent_id].eval()
 
-            # begin episode
-            for step in range(args.num_steps):
-                consensus = [0] * args.num_classes
+                # agents get to see the cropped image for partial observability
+                cropped_image = utils.crop_image(img, str(agent_id))
+                traits[agent_id], action, _, msgs_broadcast[agent_id], _ = \
+                    agents[agent_id](cropped_image,
+                                     msgs_input[agent_id])
+
+                # if episode_id == 48000:
+                #     resources_file.write('agent:' + str(agent_id) +
+                #                          ' resource allocation: ' + str(traits[agent_id][0]) + '\n')
+
+                # store the value of reputation parameter in the consensus
+                if step != 0:
+                    consensus[action.item()] += traits[agent_id][2][0].item()
+
+            # message passing
+            for agent_id in range(args.num_agents):
+                index1 = 0
+                for neighbour in range(args.num_agents):
+                    if neighbour < agent_id:
+                        msgs_input[neighbour][agent_id - 1] = msgs_broadcast[agent_id][index1].clone()
+                        index1 += 1
+                    elif neighbour > agent_id:
+                        msgs_input[neighbour][agent_id] = msgs_broadcast[agent_id][index1].clone()
+                        index1 += 1
+
+                save_msgs_broadcast[agent_id].append(torch.cat(list(msgs_broadcast[agent_id]), dim=0).detach().tolist())
+
+            # define reward function
+            if max(np.array(consensus)) > args.threshold and \
+                    torch.tensor(consensus).argmax().unsqueeze(0) == target:
+                prize = args.prize
+                num_consensus += 1
                 for agent_id in range(args.num_agents):
-
-                    # agents get to see the cropped image for partial observability
-                    cropped_image = utils.crop_image(img, str(agent_id))
-                    traits[agent_id], action, _, msgs_broadcast[agent_id], _ = \
-                        agents[agent_id](cropped_image,
-                                         msgs_input[agent_id])
-
-                    # if episode_id == 48000:
-                    #     resources_file.write('agent:' + str(agent_id) +
-                    #                          ' resource allocation: ' + str(traits[agent_id][0]) + '\n')
-
-                    # store the value of reputation parameter in the consensus
-                    if step != 0:
-                        consensus[action.item()] += traits[agent_id][2][0].item()
-
-                # message passing
+                    prize_share = (traits[agent_id][2][0].detach() / total_reputation) * prize
+                    agent_rewards[agent_id].append(prize_share.item())
+                break
+            else:
                 for agent_id in range(args.num_agents):
-                    index1 = 0
-                    for neighbour in range(args.num_agents):
-                        if neighbour < agent_id:
-                            msgs_input[neighbour][agent_id - 1] = msgs_broadcast[agent_id][index1].clone()
-                            index1 += 1
-                        elif neighbour > agent_id:
-                            msgs_input[neighbour][agent_id] = msgs_broadcast[agent_id][index1].clone()
-                            index1 += 1
+                    prize_share = (traits[agent_id][2][0].detach() / total_reputation) * (-1)
+                    agent_rewards[agent_id].append(prize_share)
 
-                # define reward function
-                if max(np.array(consensus)) > args.threshold and \
-                        torch.tensor(consensus).argmax().unsqueeze(0) == target:
-                    prize = args.prize
-                    num_consensus += 1
-                    for agent_id in range(args.num_agents):
-                        prize_share = (traits[agent_id][2][0].detach() / total_reputation) * prize
-                        agent_rewards[agent_id].append(prize_share.item())
-                    break
-                else:
-                    for agent_id in range(args.num_agents):
-                        prize_share = (traits[agent_id][2][0].detach() / total_reputation) * (-1)
-                        agent_rewards[agent_id].append(prize_share)
+        print_rewards = [torch.tensor(agent_rewards[agent_id]).sum().item()
+                         for agent_id in range(args.num_agents)]
 
-            print_rewards = [torch.tensor(agent_rewards[agent_id]).sum().item()
-                             for agent_id in range(args.num_agents)]
+        # compute mean reward of 2,000 episodes (number of test images)
+        running_rewards += np.array(print_rewards).sum()
+        if episode_id % 1999 == 0 and episode_id != 0:
+            print('[{}] rewards: {} (reached consensus: {} / 10000)'.format(episode_id,
+                                                                            running_rewards / 2000,
+                                                                            num_consensus))
+            running_rewards = 0.0
+            num_consensus = 0
 
-            # compute mean reward of 10,000 steps (number of test images)
-            running_rewards += np.array(print_rewards).sum()
-            if episode_id % 9999 == 0 and episode_id != 0:
-                print('[{}, {}] rewards: {} (reached consensus: {} / 10000)'.format(epoch, episode_id,
-                                                                                   running_rewards / 10000,
-                                                                                   num_consensus))
-                running_rewards = 0.0
-                num_consensus = 0
-
-            gc.collect()
+        # save embeddings for 1000 episodes
+        if episode_id < 1000:
+            for agent_id in agents:
+                save_embeddings(save_msgs_broadcast[agent_id], target.item(), agent_id, episode_id, policy_step)
